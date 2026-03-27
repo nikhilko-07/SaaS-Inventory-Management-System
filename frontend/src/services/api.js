996ff0d5df188e26acd45c22b9bd22653a34e8d5
Nikhil Kohli <nikhilkohli2407@gmail.com>
2026-03-28 03:24:20 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://saas-inventory-management-system-9x9y.onrender.com' || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
